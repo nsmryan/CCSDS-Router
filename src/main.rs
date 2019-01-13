13ded81b392b35f06fdb31d45758669b84238adf
U-NDC\nmryan <nmryan@LASLW16010654.ndc.nasa.gov>
2019-01-13 15:00:15 -0500

    split processing thread into processing.rs

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,9 @@ use stream::*;
 mod types;
 use types::*;
 
+mod processing;
+use processing::*;
+
 
 /// Window width given to SDL
 const WINDOW_WIDTH:  f32 = 640.0;
@@ -414,6 +417,7 @@ fn run_gui(receiver: Receiver<GuiMessage>, sender: Sender<ProcessingMsg>) {
             }
         }
 
+        /* UI */
         let ui = imgui_sdl2.frame(&window, &mut imgui, &event_pump);
 
         ui.window(im_str!(""))
@@ -778,174 +782,3 @@ fn input_string(ui: &Ui, label: &ImStr, string: &mut String, imgui_str: &mut ImS
     string.push_str(&imgui_str.to_str());
 }
 
-
-/* Packet Processing Thread */
-fn process_thread(sender: Sender<GuiMessage>, receiver: Receiver<ProcessingMsg>) {
-    let mut state: ProcessingState = ProcessingState::Idle;
-  
-    let mut packet: Packet
-        = Packet { header: Default::default(),
-                   bytes: Vec::with_capacity(4096),
-    };
-
-    let mut processing = false;
-    let mut terminating = false;
-
-    let config: AppConfig = Default::default();
-
-    let mut in_stream  = ReadStream::Null;
-    let mut out_stream = WriteStream::Null;
-
-    let mut endianness: Endianness = Endianness::Little;
-
-
-    loop {
-        match state {
-            ProcessingState::Idle => {
-                in_stream  = ReadStream::Null;
-                out_stream = WriteStream::Null;
-
-                let msg_result = receiver.recv().ok();
-                match msg_result {
-                    // Start processing from a given set of configuration settings
-                    Some(ProcessingMsg::Start(config)) => {
-                        // get endianness to use
-                        if config.little_endian_ccsds {
-                            endianness = Endianness::Little;
-                        }
-                        else {
-                            endianness = Endianness::Big;
-                        }
-
-                        // open streams
-                        match open_input_stream(&config.input_settings, config.input_selection) {
-                          Ok(stream) => {
-                              in_stream  = stream;
-
-                              match open_output_stream(&config.output_settings, config.output_selection) {
-                                Ok(stream) => {
-                                    out_stream = stream;
-                                    state = ProcessingState::Processing;
-                                },
-
-                                Err(err_string) => {
-                                    sender.send(GuiMessage::Error(err_string)).unwrap();
-                                    sender.send(GuiMessage::Finished).unwrap();
-                                },
-                              }
-                          },
-
-                          Err(err_string) => {
-                              sender.send(GuiMessage::Error(err_string)).unwrap();
-                              sender.send(GuiMessage::Finished).unwrap();
-                          },
-                        }
-                    },
-
-                    Some(ProcessingMsg::Terminate) => {
-                        state = ProcessingState::Terminating;
-                    },
-
-                    Some(msg) => {
-                        sender.send(GuiMessage::Error(format!("Unexpected message while waiting to process {}", msg.name()))).unwrap();
-                    }
-
-                    None => {
-                        // the result is not checked here because we are going to terminate whether
-                        // or not it is received.
-                        sender.send(GuiMessage::Error("Message queue error while idle".to_string())).unwrap();
-                        state = ProcessingState::Terminating;
-                    },
-                }
-            },
-
-            ProcessingState::Paused => {
-                let msg_result = receiver.recv().ok();
-                match msg_result {
-                    Some(ProcessingMsg::Continue) => {
-                        state = ProcessingState::Processing;
-                    },
-
-                    Some(ProcessingMsg::Cancel) => {
-                        state = ProcessingState::Idle;
-                    },
-
-                    Some(msg) => {
-                        sender.send(GuiMessage::Error(format!("Unexpected message while paused {}", msg.name()))).unwrap();
-                    }
-
-                    None => {
-                        // the result is not checked here because we are going to terminate whether
-                        // or not it is received.
-                        sender.send(GuiMessage::Error("Message queue error while paused".to_string())).unwrap();
-                        state = ProcessingState::Terminating;
-                    },
-                }
-            },
-
-            ProcessingState::Processing => {
-                loop {
-                    /* Check for Control Messages */
-                    let proc_msg = receiver.recv_timeout(time::Duration::from_millis(0)).ok();
-                    match proc_msg {
-                        Some(ProcessingMsg::Pause) => {
-                            state = ProcessingState::Paused;
-                            break;
-                        },
-
-                        Some(ProcessingMsg::Cancel) => {
-                            state = ProcessingState::Idle;
-                            break;
-                        },
-
-                        Some(ProcessingMsg::Terminate) => {
-                            state = ProcessingState::Terminating;
-                            break;
-                        },
-
-                        Some(msg) => {
-                            sender.send(GuiMessage::Error(format!("Unexpected message while processing {}", msg.name()))).unwrap();
-                        }
-
-                        None => {
-                            // NOTE should check for errors. ignore timeouts, but report others.
-                            // the result is not checked here because we are going to terminate whether
-                            // or not it is received.
-                            //sender.send(GuiMessage::Error("Message queue error while processing".to_string())).unwrap();
-                            //state = ProcessingState::Terminating;
-                        }
-                    }
-
-                    /* Process a Packet */
-                    match stream_read_packet(&mut in_stream, &mut packet, endianness, config.packet_size) {
-                        Err(e) => {
-                            sender.send(GuiMessage::Error(e)).unwrap();
-                            state = ProcessingState::Idle;
-                            break;
-                        },
-
-                        _ => {},
-                    }
-
-                    stream_send(&mut out_stream, &packet.bytes);
-
-                    /* Report packet to GUI */
-                    let packet_update = PacketUpdate { apid: packet.header.control.apid(),
-                                                       packet_length: packet.bytes.len() as u16,
-                                                     };
-
-                    sender.send(GuiMessage::PacketUpdate(packet_update)).unwrap();
-                }
-
-                sender.send(GuiMessage::Finished).unwrap();
-            },
-
-            ProcessingState::Terminating => {
-                break;
-            },
-        } // match state
-    } // loop
-
-    // the result is not inspected here- we are going to exit whether or not our message is received.
-    sender.send(GuiMessage::Terminate);
-}
---
+++ src/processing.rs
@@ -0,0 +1,196 @@
+use std::time;
+use std::time::Duration;
+use std::thread;
+use std::io::{Write, Read};
+use std::default::Default;
+use std::collections::HashMap;
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::fs::File;
+use std::fs::create_dir;
+
+use ccsds_primary_header::*;
+
+use byteorder::{LittleEndian, BigEndian};
+
+use simplelog::*;
+
+use chrono::prelude::*;
+
+use floating_duration::TimeAsFloat;
+
+use imgui::*;
+
+use types::*;
+use stream::*;
+
+
+/* Packet Processing Thread */
+pub fn process_thread(sender: Sender<GuiMessage>, receiver: Receiver<ProcessingMsg>) {
+    let mut state: ProcessingState = ProcessingState::Idle;
+  
+    let mut packet: Packet
+        = Packet { header: Default::default(),
+                   bytes: Vec::with_capacity(4096),
+    };
+
+    let mut processing = false;
+    let mut terminating = false;
+
+    let config: AppConfig = Default::default();
+
+    let mut in_stream  = ReadStream::Null;
+    let mut out_stream = WriteStream::Null;
+
+    let mut endianness: Endianness = Endianness::Little;
+
+
+    loop {
+        match state {
+            ProcessingState::Idle => {
+                in_stream  = ReadStream::Null;
+                out_stream = WriteStream::Null;
+
+                let msg_result = receiver.recv().ok();
+                match msg_result {
+                    // Start processing from a given set of configuration settings
+                    Some(ProcessingMsg::Start(config)) => {
+                        // get endianness to use
+                        if config.little_endian_ccsds {
+                            endianness = Endianness::Little;
+                        }
+                        else {
+                            endianness = Endianness::Big;
+                        }
+
+                        // open streams
+                        match open_input_stream(&config.input_settings, config.input_selection) {
+                          Ok(stream) => {
+                              in_stream  = stream;
+
+                              match open_output_stream(&config.output_settings, config.output_selection) {
+                                Ok(stream) => {
+                                    out_stream = stream;
+                                    state = ProcessingState::Processing;
+                                },
+
+                                Err(err_string) => {
+                                    sender.send(GuiMessage::Error(err_string)).unwrap();
+                                    sender.send(GuiMessage::Finished).unwrap();
+                                },
+                              }
+                          },
+
+                          Err(err_string) => {
+                              sender.send(GuiMessage::Error(err_string)).unwrap();
+                              sender.send(GuiMessage::Finished).unwrap();
+                          },
+                        }
+                    },
+
+                    Some(ProcessingMsg::Terminate) => {
+                        state = ProcessingState::Terminating;
+                    },
+
+                    Some(msg) => {
+                        sender.send(GuiMessage::Error(format!("Unexpected message while waiting to process {}", msg.name()))).unwrap();
+                    }
+
+                    None => {
+                        // the result is not checked here because we are going to terminate whether
+                        // or not it is received.
+                        sender.send(GuiMessage::Error("Message queue error while idle".to_string())).unwrap();
+                        state = ProcessingState::Terminating;
+                    },
+                }
+            },
+
+            ProcessingState::Paused => {
+                let msg_result = receiver.recv().ok();
+                match msg_result {
+                    Some(ProcessingMsg::Continue) => {
+                        state = ProcessingState::Processing;
+                    },
+
+                    Some(ProcessingMsg::Cancel) => {
+                        state = ProcessingState::Idle;
+                    },
+
+                    Some(msg) => {
+                        sender.send(GuiMessage::Error(format!("Unexpected message while paused {}", msg.name()))).unwrap();
+                    }
+
+                    None => {
+                        // the result is not checked here because we are going to terminate whether
+                        // or not it is received.
+                        sender.send(GuiMessage::Error("Message queue error while paused".to_string())).unwrap();
+                        state = ProcessingState::Terminating;
+                    },
+                }
+            },
+
+            ProcessingState::Processing => {
+                loop {
+                    /* Check for Control Messages */
+                    let proc_msg = receiver.recv_timeout(time::Duration::from_millis(0)).ok();
+                    match proc_msg {
+                        Some(ProcessingMsg::Pause) => {
+                            state = ProcessingState::Paused;
+                            break;
+                        },
+
+                        Some(ProcessingMsg::Cancel) => {
+                            state = ProcessingState::Idle;
+                            break;
+                        },
+
+                        Some(ProcessingMsg::Terminate) => {
+                            state = ProcessingState::Terminating;
+                            break;
+                        },
+
+                        Some(msg) => {
+                            sender.send(GuiMessage::Error(format!("Unexpected message while processing {}", msg.name()))).unwrap();
+                        }
+
+                        None => {
+                            // NOTE should check for errors. ignore timeouts, but report others.
+                            // the result is not checked here because we are going to terminate whether
+                            // or not it is received.
+                            //sender.send(GuiMessage::Error("Message queue error while processing".to_string())).unwrap();
+                            //state = ProcessingState::Terminating;
+                        }
+                    }
+
+                    /* Process a Packet */
+                    match stream_read_packet(&mut in_stream, &mut packet, endianness, config.packet_size) {
+                        Err(e) => {
+                            sender.send(GuiMessage::Error(e)).unwrap();
+                            state = ProcessingState::Idle;
+                            break;
+                        },
+
+                        _ => {},
+                    }
+
+                    stream_send(&mut out_stream, &packet.bytes);
+
+                    /* Report packet to GUI */
+                    let packet_update = PacketUpdate { apid: packet.header.control.apid(),
+                                                       packet_length: packet.bytes.len() as u16,
+                                                     };
+
+                    sender.send(GuiMessage::PacketUpdate(packet_update)).unwrap();
+                }
+
+                sender.send(GuiMessage::Finished).unwrap();
+            },
+
+            ProcessingState::Terminating => {
+                break;
+            },
+        } // match state
+    } // loop
+
+    // the result is not inspected here- we are going to exit whether or not our message is received.
+    sender.send(GuiMessage::Terminate);
+}
